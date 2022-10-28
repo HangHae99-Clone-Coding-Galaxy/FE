8d55b8cb34a42dbe5f960022bef051dfefbebd9a
Limsohee <v_vsoi@naver.com>
2022-10-29 02:13:02 +0900

    헤드 수정

--- src/Redux/config/configStore.js
+++ src/Redux/config/configStore.js
@@ -3,12 +3,10 @@ import modalSlice from "../../features/modalSlice";
 import userSlice from "../../features/userSlice";
 
 export const store = configureStore({
-<<<<<<< HEAD
-  reducer: {},
-=======
+
   reducer: {
     modalSlice,
     userSlice,
   },
->>>>>>> d4c2a637f34b7b1193c47cc704a3b11650723de7
+
 });
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 const Cart = () => {
-<<<<<<< HEAD
-  return (
-    <div>
-      <p>/장바구니 장바구니</p>
-    </div>
-  );
-=======
   return <section id="title"></section>;
->>>>>>> d4c2a637f34b7b1193c47cc704a3b11650723de7
 };
 
 export default Cart;
